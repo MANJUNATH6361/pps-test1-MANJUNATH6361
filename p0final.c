6677a9f3d82a9fbc8d464901c7aedc418fd8e9cd
Manjunatha <96220560+MANJUNATH6361@users.noreply.github.com>
2022-02-03 15:48:50 +0000

    p0final.c

--- p0final.c
+++ p0final.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main()
 {
-  int a,b,sum=0;
-  printf("enter two numbers\n");
-  scanf("%d %d",&a,&b);
-  scan=a/b;
-  printf("%d/%d=%d",a,b,sum);
+  printf("Hello,I'am Manjunatha A\n");
   return 0;
 }
