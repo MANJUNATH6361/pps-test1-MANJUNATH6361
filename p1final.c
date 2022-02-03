d515adcd3404d2dd202238ff5cbdc8d8ca57defe
Manjunatha <96220560+MANJUNATH6361@users.noreply.github.com>
2022-02-03 15:57:00 +0000

    p1original.c

---
+++ p1final.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+ int a, b;
+ printf("Enter number 1: ");
+ scanf("%d", &a);
+
+printf("Enter number 2: ");
+scanf("%d", &b);
+
+printf("The sum of %d and %d is %d\n",a, b, a + b);
+return 0;
+}
--- p1original.c
+++ p1original.c
@@ -1,2 +1,15 @@
+#include <stdio.h>
 
+int main()
+{
+ int a, b;
+ printf("Enter number 1: ");
+ scanf("%d", &a);
+
+printf("Enter number 2: ");
+scanf("%d", &b);
+
+printf("The sum of %d and %d is %d\n",a, b, a + b);
+return 0;
 }
+
